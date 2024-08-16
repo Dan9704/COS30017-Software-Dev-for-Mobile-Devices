2a35b8b1287fed86e8454752b1abf679ba68bd29
Dan9704 <116186495+Dan9704@users.noreply.github.com>
2024-08-16 19:16:20 +1000

    hi

---
+++ Week3/app/src/main/java/au/edu/swin/sdmd/iconswitcher/MainActivity.kt
@@ -0,0 +1,47 @@
+package au.edu.swin.sdmd.iconswitcher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import au.edu.swin.sdmd.iconswitcher.ui.theme.IconSwitcherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IconSwitcherTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IconSwitcherTheme {
+        Greeting("Android")
+    }
+}
